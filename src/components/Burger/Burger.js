3d41b95355807b082b3e64415cd21db211b90900
administrator <lena.petrova.v@gmail.com>
2018-12-10 11:59:32 +0000

    Burger Ingredients Dynamically (Obj to Arr)

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,28 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map( igKey => {
+            return [...Array(props.ingredients[igKey])]
+                .map( (_, i) => {
+                    console.log(" igKey, _ , i =>  ", igKey,  _, i );
+                    return <BurgerIngredient  key={igKey + i} type={igKey}/>;
+                })
+        });
+
+
+
+    console.log(" props.ingredients = ", props.ingredients);
+    console.log(" Object.keys(props.ingredients) = ", Object.keys(props.ingredients));
+    console.log(" Object.keys(props.ingredients).map( igKey => {return [...Array(props.ingredients[igKey])]} ", Object.keys(props.ingredients).map( igKey => {return [...Array(props.ingredients[igKey])]}));
+    console.log(" ", );
+    console.log(" ", );
+    console.log(" ", );
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient  type={"bread-top"}/>
-            <BurgerIngredient  type={"cheese"}/>
-            <BurgerIngredient  type={"meat"}/>
+            {transformedIngredients}
             <BurgerIngredient  type={"bread-bottom"}/>
         </div>
 
