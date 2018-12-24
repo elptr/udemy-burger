d4b55cd7354d54383270d0794334b93faf55e07c
administrator <lena.petrova.v@gmail.com>
2018-12-24 17:48:04 +0000

    adding the lifecycle methods

--- src/components/Burger/OrderSummery/OrderSummery.js
+++ src/components/Burger/OrderSummery/OrderSummery.js
@@ -1,34 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummery = (props) => {
-
-    const ingredientsSummery = Object.keys(props.ingredients)
-        .map ( igKey => {
-            return (
-                <li key={igKey}>
-                    <span style={{textTransform:'capitalize'}}>{igKey}</span>:
-                    {props.ingredients[igKey]}
-                </li>
-            );
-        });
-
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with following ingredients </p>
-            <ul>
-                {ingredientsSummery}
-            </ul>
-            <p><strong>Total Price: {props.price}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType='Danger' clicked={props.purchaseCanseled}>CANSEL</Button>
-            <Button btnType='Success' clicked={props.purchaseContinued}>CONTINUE</Button>
-
-        </Aux>
+class OrderSummery extends Component {
+    componentWillUpdate(){
+        console.log("order summery update");
+    }
+    render(){
+        const ingredientsSummery = Object.keys(this.props.ingredients)
+            .map(igKey => {
+                return (
+                    <li key={igKey}>
+                        <span style={{textTransform: 'capitalize'}}>{igKey}</span>:
+                        {this.props.ingredients[igKey]}
+                    </li>
+                );
+            });
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with following ingredients </p>
+                <ul>
+                    {ingredientsSummery}
+                </ul>
+                <p><strong>Total Price: {this.props.price}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button btnType='Danger' clicked={this.props.purchaseCanseled}>CANSEL</Button>
+                <Button btnType='Success' clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Aux>
         )
+    }
+
 };
 
 
-export default orderSummery;
+export default OrderSummery;
