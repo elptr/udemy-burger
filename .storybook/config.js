59594bdce52c3c1ef2dfd32f33a830249926fc9a
administrator <lena.petrova.v@gmail.com>
2018-12-28 11:18:04 +0000

    Changed folder structure and installed storybook

---
+++ .storybook/config.js
@@ -0,0 +1,21 @@
+// import { configure } from '@storybook/react';
+//
+// function loadStories() {
+//     require('../src/stories/index.js');
+//     // You can require as many stories as you need.
+// }
+//
+// configure(loadStories, module);
+
+
+
+
+import { configure } from '@storybook/react';
+
+const req = require.context('../src', true, /\.stories\.js$/);
+
+function loadStories() {
+    req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
 
-import Aux from '../../hoc/Aux';
+import Aux from '../Aux/Aux';
 import classes from './Layout.css';
-import Toolbar from '../Navigation/Toolbar/Toolbar';
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
     state = {
@@ -31,7 +31,6 @@ class Layout extends Component {
                     open={this.state.showSideDrawer}
                     closed={this.sideDrawerClosedHandler}
                 />
-                <div>Toolbar, SideDrawer, Backdrop</div>
                 <main className={classes.Content}>
                     {this.props.children}
                 </main>
