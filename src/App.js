3e1fb448d61a272af9712926ed5a8fbae6d585cd
administrator <lena.petrova.v@gmail.com>
2019-04-11 11:46:03 +0100

    set up routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import './App.css';
-
+import {Route, Switch} from 'react-router-dom';
 
 class App extends Component {
     render() {
         return (
             <Layout>
-                <BurgerBuilder/>
-                <Checkout />
+                <Switch>
+                    <Route path="/checkout" component={Checkout}/>
+                    <Route path="/" exact component={BurgerBuilder}/> /* put at the end to not to catch it in path*/
+                </Switch>
             </Layout>
 
         );
