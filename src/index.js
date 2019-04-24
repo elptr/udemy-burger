88418d05253a5291be0eec5179021f36f9dde84d
administrator <lena.petrova.v@gmail.com>
2019-04-24 12:42:04 +0100

    added REDUX_DEVTOOLS_EXTENSION to see states in chrome

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducer from './store/reducer';
 
-const store = createStore(reducer);
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const app = ( //() to use multi lines code
     <Provider store={store}>
