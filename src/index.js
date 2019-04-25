9d32a3901d1264e4a0e5c359b77687277e39b448
administrator <lena.petrova.v@gmail.com>
2019-04-25 17:13:45 +0100

    installed thunk, changing redux devtools var, prepare to use middleware

--- src/index.js
+++ src/index.js
@@ -7,10 +7,15 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import burgerBuilder from './store/reducers/burgerBuilder';
+import thunk from 'redux-thunk';
 
-const store = createStore(burgerBuilder, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(burgerBuilder, composeEnhancers(
+    applyMiddleware(thunk)
+));
 
 const app = ( //() to use multi lines code
     <Provider store={store}>
